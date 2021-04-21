cbd4b4c4e2325ae57b32203dd9835955fb3d88cd
deejayfakiofficial <71965798+deejayfakiofficial@users.noreply.github.com>
2021-04-21 15:08:14 +0400

    add alamofire framework

--- mvvmtest/Services/NetworkService.swift
+++ mvvmtest/Services/NetworkService.swift
@@ -5,6 +5,7 @@
 //  Created by Stanislav Terentyev on 13.04.2021.
 //
 import Foundation
+import Alamofire
 
 class NetworkService {
     
@@ -12,22 +13,18 @@ class NetworkService {
     
     private init () {}
     
-    func fetchJSON (completion: @escaping (MainModel)->Void ) {
-        guard let url = URL(string: "https://open.exchangerate-api.com/v6/latest") else { return }
-        URLSession.shared.dataTask(with: url) { (data, response, error) in
-            if error != nil {
-                print (error?.localizedDescription as Any)
-                return
-            }
-            guard let data = data else { return }
+    func fetchJSON (completion: @escaping (MainModel) -> Void) {
+        AF.request("https://open.exchangerate-api.com/v6/latest").response { responseData in
+            guard let data = responseData.data else { return }
             do {
                 let results = try JSONDecoder().decode(MainModel.self, from: data)
                 completion(results)
             }
             catch {
-                print(error.localizedDescription)
+               print("Error")
             }
-        }.resume()
+            
+        }
     }
 }
 
